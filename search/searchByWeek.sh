9f3758716160d952b57523ace1a32a6be1ef900a
YoungBrain <42530085+Khondakar@users.noreply.github.com>
2020-08-14 15:17:52 +1000

    Update searchByWeek.sh

--- search/searchByWeek.sh
+++ search/searchByWeek.sh
@@ -11,10 +11,10 @@ echo -n -e "Enter the Word you want to Search : "
 read inputword
 
 # Display the search result all time based on word search
-sudo journalctl --since "1 week ago" | grep "$inputword" > /home/khondakar/workSpace/LMS/Reports/ReportLast7Days.txt
+sudo journalctl --since "1 week ago" | grep "$inputword" > ~/workSpace/LMS/Reports/ReportLast7Days.txt
 
 # Display the result column wise with highlighted searching key word
-column -t /home/khondakar/workSpace/LMS/Reports/ReportLast7Days.txt | grep --color=always "$inputword" | more
+column -t ~/workSpace/LMS/Reports/ReportLast7Days.txt | grep --color=always "$inputword" | more
 
 echo "*----------------- End of Search Result! -----------------------*"
 
